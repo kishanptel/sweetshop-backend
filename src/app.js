bd276d59600fbddcea00bcd9d6323597a8c3fff7
Kishan <kishanptel0709@gmail.com>
2026-07-13 00:52:48 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -38,7 +38,9 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'))
         }
     },
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.get("/", (req, res) => {
